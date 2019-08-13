446b28f05bf7fedb486eb925cd3f1020ab538950
Cherish599 <1146360747@qq.com>
2019-08-13 11:15:03 +0800

    新增登录功能

---
+++ cherish599/cherish599/login.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace huodong
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            activityFirst activityFirst = new activityFirst();
+            activityFirst.Show();
+            //this.Hide();
+        }
+    }
+}
